6c8c892a5e9f9bfbbbead89c137708280efa339a
cosineta <18710960952@yeah.net>
2019-10-11 22:18:28 +0800

    create

---
+++ sfp/ch1/stdio.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+	std::cout<<"Enter ";
+	std::cout<<"two numbers:";
+	std::cout<<std::endl;
+	int v1,v2;
+	std::cin >> v1 >> v2;
+	std::cout << "the sum of " <<v1<<" and "<<v2<<" is "<< v1+v2 <<std::endl;
+
+	return 0;
+}
