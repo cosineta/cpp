f83752c9eb1f971f1c9193bd7cb5306a578d25b4
cosineta <18710960952@yeah.net>
2019-10-29 23:15:43 +0800

    ch5 is over

---
+++ sfp/ch5/5.23.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+using std::vector;
+int main()
+{
+	int a,b;
+	float c;
+	cin >> a >> b;
+	c=(float)a/b;
+
+	cout << c << endl;
+
+
+
+
+	return 0;
+}
